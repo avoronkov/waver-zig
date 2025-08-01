c7692be3e82bcc0e934009f9a421e042cebd21f1
Alexander Voronkov <alxn1101r@gmail.com>
2025-08-01 16:00:02 +0500

    Add basic waveforms, update README

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # waver-zig
 
 Zig implementation of a musical programming language Pelia.
+
+## TODO
+
+[ ] "repeat" function
+
+[ ] "loop" function
+
+[ ] "tempo" pragma
+
+[ ] wave filters...
+
+[ ] edo19
+
+[ ] euclidian rhythms
+
+[ ] embedeb samples
+
+[ ] srand pragma
+
+[ ] noise waveform
--- src/waveform.zig
+++ src/waveform.zig
@@ -12,6 +12,52 @@ pub fn sine(t: f64, note: Note) !f64 {
     return res;
 }
 
+pub fn triangle(t: f64, note: Note) !f64 {
+    if (t > note.dur) {
+        return error.Eof;
+    }
+    // Start from 0-point.
+    const y = (t * note.freq) + 0.25;
+    const yd = y - std.math.floor(y);
+    return if (yd < 0.5)
+        -1 + 4 * yd
+    else
+        1 - 4 * (yd - 0.5);
+}
+
+pub fn square(t: f64, note: Note) !f64 {
+    if (t > note.dur) {
+        return error.Eof;
+    }
+
+    const y = t * note.freq;
+    return if (y - std.math.floor(y) < 0.5) 1 else -1;
+}
+
+pub fn saw(t: f64, note: Note) !f64 {
+    if (t > note.dur) {
+        return error.Eof;
+    }
+
+    const y = t * note.freq + 0.25;
+    const yd = y - std.math.floor(y);
+    return yd * 2 - 1;
+}
+
+pub fn semisine(t: f64, note: Note) !f64 {
+    if (t > note.dur) {
+        return error.Eof;
+    }
+
+    const x = 2 * std.math.pi * note.freq * t;
+    return 2 * @abs(std.math.sin(x)) - 1;
+}
+
+
 pub const waveforms = std.static_string_map.StaticStringMap(WaveForm).initComptime(.{
     .{ "sine", sine },
+    .{ "triangle", triangle },
+    .{ "square", square },
+    .{ "saw", saw },
+    .{ "semisine", semisine },
 });
